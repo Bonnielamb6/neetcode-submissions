f115ee5784e5ffa485f3781108ea6651fb2ffb23
Bonnielamb6 <94414803+Bonnielamb6@users.noreply.github.com>
2026-07-24 09:54:27 -0600

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Deque<Integer> indices = new ArrayDeque<>();
+        int[] result = new int[temperatures.length];
+        for(int actual = 0 ; actual < temperatures.length ; actual++){
+            if(actual > 0 ){
+                while(!indices.isEmpty() && temperatures[indices.peek()] < temperatures[actual]){
+                    int tempIndex = indices.pop();
+                    result[tempIndex] = actual - tempIndex;
+                }
+            }
+            indices.push(actual);
+        }
+        for(int actual : indices){
+            result[actual] = 0;
+        }
+        return result;
+    }
+}
